fca3d62d8445ab27b7ad0bcad508e2bca668da9e
Sanyam Goel <148613715+hexwhiz@users.noreply.github.com>
2025-01-31 21:48:59 +0530

    fixed the bug with udp data buffer

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,8 @@ from cvzone.HandTrackingModule import HandDetector
 import socket
 
 # dimensions
-height = 720
-width = 1280
+height = 1080
+width = 1920
 
 cap = cv2.VideoCapture(0)
 cap.set(3,width)
@@ -18,19 +18,21 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddPort = ("127.0.0.1", 5020)
 
 # Using the webcam
-data = []
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
-    # if hands and hands[0]['lmList'][8][0] < 0:
-    # exit(0)
 
-    # show camera output with tracking
+    # # for testing: touch left edge of screen with index finger to close
+    # if hands and hands[0]['lmList'][8][0] < 0:
+    #   exit(0)
+
+    # # show camera output with tracking
     # cv2.imshow("Image", img)
     cv2.waitKey(1)
 
     # generating landmark data
     if hands:
+        data = []
         hand = hands[0]
         lmList = hand['lmList']
         for lm in lmList:
@@ -38,4 +40,6 @@ while True:
         
         # sending data to udp server
         sock.sendto(str.encode(str(data)), serverAddPort)
-        print(data)
+
+        # # Log data to console
+        # print(data)
