c22d89e0e07627c43f73fb999a06449c5263d4ee
Sanyam Goel <148613715+hexwhiz@users.noreply.github.com>
2025-02-08 05:47:56 +0530

    freeplay ui and control changes

--- Unity/Assets/Scripts/FreePlay.cs
+++ Unity/Assets/Scripts/FreePlay.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class FreePlay : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private string previousValue;
+    private Coroutine checkRoutine;
+    public float delay = 1.5f;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // if game is in level then check for udp data stability for pausing
+        if (GameManager.state == GameManager.GameState.free && UDPManager.Instance.GetData() != previousValue)
+        {
+            previousValue = UDPManager.Instance.GetData();
+            if (checkRoutine != null)
+                StopCoroutine(checkRoutine);
+            checkRoutine = StartCoroutine(WaitForStability());
+        }
+    }
+
+    public void ExitFreePlay()
+    {
+        GameManager.state = GameManager.GameState.menu;
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator WaitForStability()
+    {
+        yield return new WaitForSeconds(delay);
+        ExitFreePlay();
     }
 }
